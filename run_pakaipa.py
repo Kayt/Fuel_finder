54ae79ec5f60510428b5478ab9cd5c1b987af595
Achim Munene <achim_munene@hotmail.com>
2018-10-26 09:48:02 +0200

    this is it

---
+++ run_pakaipa.py
@@ -0,0 +1,9 @@
+import logging
+
+print("this should wourjk")
+
+from pakaipa.bot import Bot
+
+if __name__ == "__main__":
+    logging.info("Bot is active, scan your QR code from your phone's WhatsApp")
+    Bot()
