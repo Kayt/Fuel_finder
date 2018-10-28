ce3a1ae58b393dc2e8d8a3bdd1d41574f373d97d
developerprince <N01416165H@students.nust.ac.zw>
2018-10-28 10:55:10 +0200

    inprogress

--- Bot_Test.py
+++ Bot_Test.py
@@ -1,3 +1,19 @@
 import unittest 
-import fuel.helper_function
+import fuel.bot
+
+result1 = [["Fuel Station: "+data[0] + "\n" + "Fuel Type: " +data[2] + "\n"  + "Queue Length: " + data[4] + "\n" + "Location: " + data[5]+ " "+data[6] + "\"Fuel Station: "+data[0] + "\n" + "Fuel Type: " +data[2] + "\n"  + "Queue Length: " + data[4] + "\n" + "Location: " + data[5]+ " "+data[6] + "\n "
+
+class BotTest(unittest.TestCase):
+  def test_find_fuel(self):
+    self.assertEqual(find_fuel('Mount Plesant', data), result1)
+    self.assertEqual(find_fuel('Avondale', data), result2)
+    self.assertEqual(find_fuel('Glendale', data), result3)
+    self.assertEqual(find_fuel('Mkoba', data), result4)
+    
+    
+    
+    
+  
+
+
 
